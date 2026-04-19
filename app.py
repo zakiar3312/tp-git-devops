b58300714b2021c2c10fbb33ed46fe7032b862d8
zakiar <zakiaribi3312@gmail.com>
2026-04-19 20:02:05 +0100

    feat: add saluer and additionner functions

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello DevOps!")
+def saluer(nom):
+return f"Bonjour, {nom} !"
+def additionner(a, b):
+    return a + b
+def saluer(nom):
+    return f"Bonjour, {nom} !"
+if __name__ == "__main__":
+    print(saluer("DevOps"))
